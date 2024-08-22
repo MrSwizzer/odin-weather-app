3342773a0fe74a70dde76cae61ecb94027e206db
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-08-22 13:42:56 +0200

    Initial API fetch setup

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('Hello World');
+
+async function getWeather(location) {
+	try {
+		const response = await fetch(
+			`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=DPBTZYUK4L6UUS6PNS4DLRLPL&contentType=json`
+		);
+		const weatherData = await response.json();
+		console.log(weatherData);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+getWeather('Stuttgart');
